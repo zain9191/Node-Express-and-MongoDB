dd152b730c56ce022b160c8a2a977416f744529f
Zain <zain.frayha@gmail.com>
2024-01-23 13:56:11 +0100

    Modifiez la route PUT,getting errors need fix

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -22,12 +22,27 @@ const Thing = require ("../models/Thing")
 
 
 
-  exports.modifyThing = (req,res,next)=>{
-    Thing.findOne({_id: req.params._id})
-    .then(thing => res.status(201).json(thing))
-    .catch(error => res.status(404).json({error}))
-    
-  };
+  exports.modifyThing = (req, res, next) => {
+    const thingObject = req.file ? {
+        ...JSON.parse(req.body.thing),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+  
+    delete thingObject._userId;
+    Thing.findOne({_id: req.params.id})
+        .then((thing) => {
+            if (thing.userId != req.auth.userId) {
+                res.status(401).json({ message : 'Not authorized'});
+            } else {
+                Thing.updateOne({ _id: req.params.id}, { ...thingObject, _id: req.params.id})
+                .then(() => res.status(200).json({message : 'Objet modifié!'}))
+                .catch(error => res.status(401).json({ error }));
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error });
+        });
+ };
 
   exports.deleteThing =  (req, res, next) => {
     Thing.deleteOne({_id: req.params._id})
