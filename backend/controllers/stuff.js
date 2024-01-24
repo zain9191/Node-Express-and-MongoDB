286ef2bcd5dbdf9b86d08f044f0360f5ec999ac0
Zain <zain.frayha@gmail.com>
2024-01-24 16:21:34 +0100

    Modifiez la route DELETE

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,75 +1,62 @@
-const { json } = require("express");
-const Thing = require ("../models/Thing")
+// in controllers/stuff.js
 
+const Thing = require('../models/Thing');
+//********fs ---> file system ----> système de fichiers */
+//****Il nous donne accès aux fonctions qui nous permettent de
+// modifier le système de fichiers, y compris aux fonctions permettant de supprimer les fichiers. */
+const fs = require ('fs');
 
-
-  exports.createThing = (req, res, next) => {
-    const thingObject = JSON.parse(req.body.thing);
-    delete thingObject._id;
-    delete thingObject._userId;
-    const thing = new Thing({
-        ...thingObject,
-        userId: req.auth.userId,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    });
-
-    thing.save()
-    .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
-    .catch(error => { res.status(400).json({error})})
-  };
-
-
-
-
-  exports.modifyThing = (req, res, next) => {
-    const thingObject = req.file ? {
-        ...JSON.parse(req.body.thing),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : { ...req.body };
-  
-    delete thingObject._userId;
-    Thing.findOne({_id: req.params.id})
-        .then((thing) => {
-            if (thing.userId != req.auth.userId) {
-                res.status(401).json({ message : 'Not authorized'});
-            } else {
-                Thing.updateOne({ _id: req.params.id}, { ...thingObject, _id: req.params.id})
-                .then(() => res.status(200).json({message : 'Objet modifié!'}))
-                .catch(error => res.status(401).json({ error }));
-            }
-        })
-        .catch((error) => {
-            res.status(400).json({ error });
-        });
- };
-
- 
-
-  exports.deleteThing =  (req, res, next) => {
-    Thing.deleteOne({_id: req.params._id})
-        .then(() => res.status(200).json({message: "L'objet est bien supprimé"})) 
-        .catch(error => res.status(400).json({error}))
+//**********CREER NOUVEL OBJ ****************************************/
+exports.createThing = (req, res, next) => {
+  const thingObject = JSON.parse(req.body.thing);
+  delete thingObject._id;
+  const thing = new Thing({
+    ...thingObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    
+  });
+  thing.save()
+  .then(() => {res.status(201).json({message: 'Post saved successfully!'});})
+  .catch((error) => {res.status(400).json({error: error});});
 };
 
 
-  exports.getOneThing = (req,res,next)=>{
-    Thing.updateOne({_id: req.params._id}, {...req.body,_id:req.params._id})
-    .then(thing => res.status(201).json(thing))
-    .catch(error => res.status(400).json({error}))
-  };
+//************************MODIFIER OBJET *****************************/
+exports.modifyThing =  (req, res, next) => {
+  const thingObject = req.file ?
+  { 
+    ...JSON.parse(req.body.thing),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body };
+    Thing.updateOne({_id: req.params.id}, { ...thingObject, _id: req.params.id })
+    .then(() => {res.status(201).json({message: 'Objet modifié!'});})
+    .catch((error) => {res.status(400).json({error: error});});
+  }
 
-
-
-
+//*******************SUPPRIMER OBJ ****************************************/
+exports.deleteThing = (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => {
+      const filename = thing.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Thing.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+          .catch(error => res.status(400).json({ error }));
+      });
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+//*****************Récupérer 1 seul obj ***********************************/
+  exports.getOneThing = (req, res, next) => {
+    Thing.findOne({_id: req.params.id})
+    .then((thing) => {res.status(200).json(thing);})
+    .catch((error) => {res.status(404).json({error: error});
+  });
+  }
+ 
+  //*************Récupérer TOUS LES OBJ ***********************************/
   exports.getAllThings = (req, res, next) => {
-    // Check if the request is authenticated
-    if (!req.auth || !req.auth.userId) {
-      return res.status(401).json({ message: 'Unauthorized access!!!!' });
-    }
-  
-    // Now you can proceed to fetch and return the list of things
-    Thing.find()
-      .then(things => res.status(200).json(things))
-      .catch(error => res.status(400).json({ error }));
-  };
-  
+   Thing.find()
+  .then((things) => {res.status(200).json(things);})
+  .catch((error) => { res.status(400).json({ error: error});});
+}
--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,64 +1,20 @@
-// const multer = require('multer');
-
-// const MIME_TYPES = {
-//   'image/jpg': 'jpg',
-//   'image/jpeg': 'jpg',
-//   'image/png': 'png'
-// };
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, callback) => {
-//     callback(null, 'images');
-//   },
-//   filename: (req, file, callback) => {
-//     const name = file.originalname.split(' ').join('_');
-//     const extension = MIME_TYPES[file.mimetype];
-//     callback(null, name + Date.now() + '.' + extension);
-//   }
-// });
-
-// module.exports = multer({storage: storage}).single('image');
-
-
-
 const multer = require('multer');
 
-const MIME_TYPES = {
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
-  'image/png': 'png'
+const MIME_TYPES = { 
+    'iamge/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png'
 };
 
 const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    console.log('Destination:', 'images');
-    callback(null, 'images');
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    const fileName = name + Date.now() + '.' + extension;
-    console.log('Generated FileName:', fileName);
-    callback(null, fileName);
-  }
+    destination: (req, file, callback) => {
+        callback(null, 'images')
+    },
+    filename: (req, file, callback) => {
+        const name = file.originalname.split(' ').join('_');
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, name + Date.now() + '.' + extension);
+    }
 });
 
-const multerMiddleware = multer({ storage: storage }).single('image');
-
-// Debugging middleware
-function debugMiddleware(req, res, next) {
-  console.log('Incoming Request:', req.method, req.url);
-  next();
-}
-
-module.exports = (req, res, next) => {
-  debugMiddleware(req, res, () => {
-    multerMiddleware(req, res, (err) => {
-      if (err) {
-        console.error('Multer Error:', err);
-        return res.status(500).json({ error: 'File Upload Error' });
-      }
-      next();
-    });
-  });
-};
+module.exports = multer({ storage }).single('image');
